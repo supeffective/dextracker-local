d0ab7ab5460c9daedb9147baa43feab615a0747c
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-01-17 23:48:30 +0100

    feat: compile native desktop apps using Tauri

--- src/components/layout/StatefulAppLayout.tsx
+++ src/components/layout/StatefulAppLayout.tsx
@@ -1,9 +1,38 @@
 import ErrorBoundary from '@/components/errors/ErrorBoundary'
 import StatelessAppLayout from '@/components/layout/StatelessAppLayout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { invoke } from '@tauri-apps/api/tauri'
+import { useState } from 'react'
 import { ErrorBoundaryFallback } from '../errors/ErrorBoundaryFallback'
 
 const queryClient = new QueryClient()
+
+export function TauriDemo() {
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke('greet', { name }))
+  }
+  return (
+    <div>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault()
+          greet()
+        }}
+      >
+        <input id="greet-input" onChange={(e) => setName(e.currentTarget.value)} placeholder="Enter a name..." />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  )
+}
 
 // Stateful App layout, used as a wrapper of every page, unless they have an exported component named "layout",
 // then, that custom layout component will be used instead.
--- vite.config.ts
+++ vite.config.ts
@@ -23,9 +23,40 @@ const plugins: PluginOption[] = [
 export default defineConfig({
   plugins: plugins,
   esbuild: { legalComments: 'none' },
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1510,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
     },
+  },
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: [
+    'VITE_',
+    'TAURI_PLATFORM',
+    'TAURI_ARCH',
+    'TAURI_FAMILY',
+    'TAURI_PLATFORM_VERSION',
+    'TAURI_PLATFORM_TYPE',
+    'TAURI_DEBUG',
+  ],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari16',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 })
